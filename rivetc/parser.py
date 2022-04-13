f351d4de41b3b4dc9296fb9ab041a3070559d2dc
StunxFS <advancestunx@hotmail.com>
2022-04-13 15:37:18 -0400

    [parser] parse attrs

--- rivetc/parser.py
+++ rivetc/parser.py
@@ -3,6 +3,7 @@
 # that can be found in the LICENSE file.
 
 from lark import Lark
+from lark.exceptions import UnexpectedInput, UnexpectedToken, UnexpectedCharacters
 
 from .utils import eprint
 
@@ -10,7 +11,8 @@ Parser = Lark(
     r"""
 module: declaration*
 
-declaration: mod_decl | fn_decl
+attrs: "@" "[" WORD (";" WORD)* "]"
+declaration: attrs* (mod_decl | fn_decl)
 
 mod_decl: "pub"? "mod" WORD "{" declaration* "}"
 fn_decl: "pub"? "fn" WORD "(" ")" "{" "}"
@@ -29,11 +31,23 @@ MULTI_COMMENT: "/*" /.*/s "*/"
     parser="lalr",
 )
 
-
 def parse(filename: str):
-    try:
-        return Parser.parse(open(filename, "r").read())
-    except Exception as err:
-        # Currently the error msgs are ugly, but they work!
-        eprint(f"{filename}:{err.line}:{err.column}: error: {err}")
-        exit(1)
+    with open(filename, "r") as f:
+        src= f.read()
+        try:
+            return Parser.parse(src)
+        except UnexpectedInput as err:
+            ctx = err.get_context(src).strip()
+            start = f"{filename}:{err.line}:{err.column}: error:"
+            if isinstance(err, UnexpectedToken):
+                eprint(f"{start} unexpected token {err.token}")
+                eprint(ctx)
+                eprint(f"expected one of: {err.expected}")
+            elif isinstance(err, UnexpectedCharacters):
+                eprint(f"{start} unexpected character")
+                eprint(ctx)
+            else:
+                eprint(f"{start} unexpected end of file")
+                eprint(ctx)
+                eprint(f"expected one of: {err.expected}")
+            exit(1)
