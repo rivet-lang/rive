39953891c461521fe26fd0935ae1aec8e044f084
StunxFS <advancestunx@hotmail.com>
2022-07-19 12:44:41 -0400

    fix(tests): fix

--- tests/run_failing_tests.py
+++ tests/run_failing_tests.py
@@ -26,7 +26,7 @@ def run_fail_tests():
 		    file, "-cc", CC
 		)
 		try:
-			outf = open(file.replace(".ri", ".out"), "rb").read()
+			outf = open(file.replace(".ri", ".out"), encoding = 'UTF-8').read()
 			if outf.strip() == res.err:
 				utils.eprint(utils.bold(utils.green(" [ PASS ] ")), file)
 			else:
