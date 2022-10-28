2374ed225a9d840acd3d1068f31348ffc1c4e68a
StunxFS <advancestunx@hotmail.com>
2022-10-28 14:45:08 -0400

    codegen: generate vtables

--- rivetc/src/codegen/c.py
+++ rivetc/src/codegen/c.py
@@ -27,6 +27,9 @@ class CGen:
     def __init__(self, comp):
         self.comp = comp
         self.typedefs = utils.Builder()
+        self.structs = utils.Builder()
+        self.protos = utils.Builder()
+        self.globals = utils.Builder()
         self.out = utils.Builder()
 
     def gen(self, out_rir):
@@ -39,6 +42,9 @@ class CGen:
         with open(c_file, "w+") as out:
             out.write(c_headers.HEADER)
             out.write(str(self.typedefs).strip() + "\n\n")
+            out.write(str(self.structs).strip() + "\n\n")
+            out.write(str(self.protos).strip() + "\n\n")
+            out.write(str(self.globals).strip() + "\n\n")
             out.write(str(self.out).strip())
 
         args = [
@@ -75,18 +81,18 @@ class CGen:
         for s in structs:
             self.typedefs.writeln(f"typedef struct {s.name} {s.name};")
             if not s.is_opaque:
-                self.writeln(f"struct {s.name} {{")
+                self.structs.writeln(f"struct {s.name} {{")
                 for i, f in enumerate(s.fields):
-                    self.write("  ")
-                    self.write_type(f.typ, f.name)
+                    self.structs.write("  ")
+                    self.structs.write(self.gen_type(f.typ, f.name))
                     if not isinstance(f.typ, (ir.Array, ir.Function)):
-                        self.write(f" {f.name}")
+                        self.structs.write(f" {f.name}")
                     if i < len(s.fields) - 1:
-                        self.writeln(";")
+                        self.structs.writeln(";")
                     else:
-                        self.writeln(";")
-                self.writeln("};")
-            self.writeln()
+                        self.structs.writeln(";")
+                self.structs.writeln("};")
+            self.structs.writeln()
 
     def gen_externs(self, externs):
         pass
@@ -97,37 +103,77 @@ class CGen:
     def gen_decls(self, decls):
         for decl in decls:
             if isinstance(decl, ir.FnDecl):
-                self.write_type(decl.ret_typ)
-                self.write(f" {decl.name}(")
-                for i, arg in enumerate(decl.args):
-                    self.write_type(arg.typ, arg.name)
-                    if not isinstance(arg.typ, (ir.Array, ir.Function)):
-                        self.write(f" {arg.name}")
-                    if i < len(decl.args) - 1:
-                        self.write(", ")
+                if isinstance(decl.ret_typ, ir.Type) and decl.ret_typ.name=="never":
+                    self.write("RIVET_NEVER ")
+                    self.protos.write("RIVET_NEVER ")
+                if decl.is_pub:
+                    self.write("RIVET_EXPORT ")
+                    self.protos.write("RIVET_EXPORT ")
+                else:
+                    self.write("RIVET_LOCAL ")
+                    self.protos.write("RIVET_LOCAL ")
+                ret_typ=self.gen_type(decl.ret_typ)
+                self.protos.write(f"{ret_typ} {decl.name}(")
+                self.write(f"{ret_typ} {decl.name}(")
+                if len(decl.args)==0:
+                    self.write("void")
+                    self.protos.write("void")
+                else:
+                    for i, arg in enumerate(decl.args):
+                        arg_typ=self.gen_type(arg.typ, arg.name)
+                        self.protos.write(arg_typ)
+                        self.write(arg_typ)
+                        if not isinstance(arg.typ, (ir.Array, ir.Function)):
+                            self.protos.write(f" {arg.name}")
+                            self.write(f" {arg.name}")
+                        if i < len(decl.args) - 1:
+                            self.protos.write(", ")
+                            self.write(", ")
+                self.protos.writeln(");")
                 self.writeln(") {")
                 self.writeln("}")
             else:
-                pass
+                self.globals.writeln(
+                    f"static {decl.structure} {decl.name}[{decl.implement_nr}] = {{"
+                )
+                for i, ft in enumerate(decl.funcs):
+                    self.globals.writeln('  {')
+                    for f, impl in ft.items():
+                        self.globals.writeln(f'    .{f} = (void*){impl}')
+                    self.globals.write("  }")
+                    if i < len(decl.funcs) - 1:
+                        self.globals.writeln(",")
+                    else:
+                        self.globals.writeln()
+                self.globals.writeln("};")
             self.writeln()
 
     def write_type(self, typ, wrap = ""):
+        self.write(self.gen_type(typ, wrap))
+
+    def gen_type(self, typ, wrap = ""):
         if isinstance(typ, ir.Pointer):
-            self.write_type(typ.typ, wrap)
-            self.write("*")
+            sb=utils.Builder()
+            sb.write(self.gen_type(typ.typ, wrap))
+            sb.write("*")
+            return str(sb)
         elif isinstance(typ, ir.Array):
-            self.write_type(typ.typ)
+            sb=utils.Builder()
+            sb.write(self.gen_type(typ.typ))
             if len(wrap) > 0:
-                self.write(f" {wrap}")
-            self.write(f"[{typ.size}]")
+                sb.write(f" {wrap}")
+            sb.write(f"[{typ.size}]")
+            return str(sb)
         elif isinstance(typ, ir.Function):
-            self.write_type(typ.ret_typ)
-            self.write("(*")
+            sb=utils.Builder()
+            sb.write(self.gen_type(typ.ret_typ))
+            sb.write("(*")
             if len(wrap) > 0:
-                self.write(wrap)
-            self.write(")(")
+                sb.write(wrap)
+            sb.write(")(")
             for i, arg in enumerate(typ.args):
-                self.write_type(arg)
-            self.write(")")
+                sb.write(self.gen_type(arg))
+            sb.write(")")
+            return str(sb)
         else:
-            self.write(str(typ))
+            return str(typ)
