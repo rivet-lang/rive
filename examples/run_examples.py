109ef2f97495110caa450877a92551329101a9ee
Jose Mendoza <56417208+StunxFS@users.noreply.github.com>
2023-11-05 07:47:18 -0400

    fix(examples): fix run_examples.py script

--- examples/run_examples.py
+++ examples/run_examples.py
@@ -49,13 +49,13 @@ def green(msg):
 def bold(msg):
 	return format(msg, "1", "22")
 
-def run_samples():
+def run_examples():
 	ok, fail = 0, 0
 	exit_code = 0
 
-	SAMPLES = glob.glob(os.path.join("samples", "*.ri"))
-	for i, file in enumerate(SAMPLES):
-		start = f" [{i+1}/{len(SAMPLES)}]"
+	EXAMPLES = glob.glob(os.path.join("examples", "*.ri"))
+	for i, file in enumerate(EXAMPLES):
+		start = f" [{i+1}/{len(EXAMPLES)}]"
 		res = run_process(sys.executable, "rivetc", file)
 		if res.exit_code == 0:
 			res = run_process("./" + filename(file))
@@ -69,13 +69,13 @@ def run_samples():
 		else:
 			eprint(start, file, bold(red("-> FAIL")))
 			fail += 1
-	eprint(bold("Summary for all samples: "), end = "")
+	eprint(bold("Summary for all examples: "), end = "")
 	if ok > 0:
 		eprint(bold(green(f"{ok} passed")) + ", ", end = "")
 	if fail > 0:
 		eprint(bold(red(f"{fail} failed")) + ", ", end = "")
-	eprint(bold(f"{len(SAMPLES)} total."))
+	eprint(bold(f"{len(EXAMPLES)} total."))
 
 	return exit_code
 
-exit(run_samples())
+exit(run_examples())
