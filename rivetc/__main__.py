c2a7897f1b6980818ab00c353a098b3101e9c34f
Jose Mendoza <56417208+StunxFS@users.noreply.github.com>
2024-09-12 14:30:35 +0000

    remove rivetc directory

--- rivetc/__main__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2024 Jose Mendoza. All rights reserved.
-# Use of this source code is governed by an MIT license that can
-# be found in the LICENSE file.
-
-import sys
-
-from src import Compiler
-
-Compiler(sys.argv[1:]).run()
