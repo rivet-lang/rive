50f43eb83300d1cc45d3bebb582c7917895b7300
StunxFS <mendozadiazjose77@gmail.com>
2024-11-14 12:01:15 -0400

    again, use V

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	@echo "Generating C code from Fut code..."
-	@fut -o fut-out/rivetc.c src/*.fu
-	@echo "Compiling C code..."
-	@gcc -o rivetc fut-out/* -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -lglib-2.0
---
+++ src/util/util.v
@@ -0,0 +1,52 @@
+// Copyright (C) 2024-present Jose Mendoza - All rights reserved. Use of this
+// source code is governed by an MIT license that can be found in the LICENSE
+// file.
+
+module util
+
+import term
+
+enum MsgLevel {
+	note
+	warn
+	error
+	ice
+}
+
+@[inline]
+fn format_msg(msg string, level MsgLevel) string {
+	return match level {
+		.note {
+			"${term.bold('rivetc: ')}${term.bold(term.cyan('error:'))} ${msg}"
+		}
+		.warn {
+			"${term.bold('rivetc: ')}${term.bold(term.yellow('error:'))} ${msg}"
+		}
+		.error {
+			"${term.bold('rivetc: ')}${term.bold(term.red('error:'))} ${msg}"
+		}
+		.ice {
+			"${term.bold('rivetc: ')}${term.bold(term.red('internal compiler error:'))} ${msg}"
+		}
+	}
+}
+
+pub fn note(msg string) {
+	eprintln(format_msg(msg, .note))
+}
+
+pub fn warn(msg string) {
+	eprintln(format_msg(msg, .warn))
+}
+
+@[noreturn]
+pub fn error(msg string) {
+	eprintln(format_msg(msg, .error))
+	exit(101)
+}
+
+@[noreturn]
+pub fn ic_error(msg string) {
+	eprintln(format_msg(msg, .ice))
+	exit(102)
+}
