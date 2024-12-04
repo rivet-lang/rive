c6db77f37a6f60ec8c29ba56cb0f5a95256376b1
StunxFS <mendozadiazjose77@gmail.com>
2024-12-04 13:57:05 -0400

    update copyright header

--- compiler/context/mod.v
+++ compiler/context/mod.v
@@ -1,6 +1,5 @@
-// Copyright (C) 2024-present Jose Mendoza - All rights reserved. Use of this
-// source code is governed by an MIT license that can be found in the LICENSE
-// file.
+// Copyright (C) 2024-present The Rivet programming language. Use of this source code
+// is governed by an MIT license that can be found in the LICENSE file.
 
 module context
 
